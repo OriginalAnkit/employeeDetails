f372ccbd88d9e1e39771781a3e8847e767f90d37
Ankit Halder <ankithalder99@gmail.com>
2019-07-19 01:03:31 +0530

    deploy

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const userRoutes=require('./routes/user.routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname +'./employeeFront/dist/employeeFront')))
 
 app.use('/api',userRoutes);
+app.use(express.static(path.join(__dirname +'public')))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './employeeFront/dist/employeeFront/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 
