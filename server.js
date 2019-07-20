4ed176246888b9e2b6fff9b0df8ccf900fdaf27d
Ankit Halder <ankithalder99@gmail.com>
2019-07-20 10:20:39 +0530

    deployed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const bodyParser= require('body-parser');
 const cors = require('cors');
+const path=require('path');
 
 const app=express();
 const setup = require('./utilities/setup');
@@ -10,15 +11,18 @@ const userRoutes=require('./routes/user.routes');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname ,'./employeeFront/dist/employeeFront')))
 
 app.use('/api',userRoutes);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './employeeFront/dist/employeeFront/index.html'));
+})
 
 
 
 
 
-
-const port=3000;
+const port = process.env.PORT ||3000;
 app.listen(port,()=>{
     console.log(`*****************Listening at port ${port}****************`)
 })
